e1f5e8fc58a89bffb4078a5b29c3db4d8b245bc5
mehdi el-hajjame <el-hajjame@outlook.com>
2026-02-20 12:47:56 +0000

    controller wallet logic has been done

---
+++ controllers/wallets.js
@@ -0,0 +1,49 @@
+const { wallets, users } = require('../data/store');
+
+const getWallets = (req, res) => {
+  res.end(JSON.stringify(wallets));
+};
+
+const createWallets = (req, res, body) => {
+  const userExist = users.find(u => u.id === body.userId);
+
+  if (!userExist) {
+    res.statusCode = 400;
+    return res.end(JSON.stringify({ message: "user not found!" }))
+  }
+
+  const wallet = {
+    id: Date.now(),
+    userId: body.userId,
+    name: body.name,
+    balance: 0
+  }
+  wallets.push(wallet);
+  res.end(JSON.stringify(wallet));
+};
+
+
+const deposit = (res, req, body) => {
+  const wallet = wallets.find(w => w.id === body.walletId);
+
+  if (!wallet || body.amount <= 0) {
+    res.statusCode = 400;
+    return res.end(JSON.stringify({ message: "error wallet not found or the amount less the zero!" }));
+
+    wallet.balance += body.amount;
+    res.end(JSON.stringify(wallet));
+  }
+}
+
+const withdraw = (req, res, body) => {
+  const wallet = wallets.find(w => w.id === body.walletId);
+
+  if (!wallet || body.amount <= 0 || wallet.balance < body.amount) {
+    res.statusCode = 400;
+    return res.end(JSON.stringify({ message: "error the wallet doesnt exist or there is no sold on this wallet!" }));
+    wallet.balance -= body.amount;
+    res.end(JSON.stringify(wallet))
+  }
+}
+
+module.exports = { getWallets, createWallet, deposit, withdraw };
--- server.js
+++ server.js
@@ -1,12 +1,15 @@
-const http = require("http");
+const http = require("http"); // built in node js module
 
-
+// this function run every time  when u visit localhost..
 const server = http.createServer((req, res) => {
     res.setHeader('Content-type', 'application/json');
     let body = '';
+    // node js processes incomig data in stream if usser sends large file it devide it into small pieces, node doesnt wait for the whole thing 
+    // req.on this is event listener means whatever a piece of data coming run this code    
     req.on('data', chunk => {
         body = body + chunk;
     });
+    // when all data arrived 
     req.on('end', () => {
         body = body ? JSON.parse(body) : {};
     })
